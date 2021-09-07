323cae6440870b43dfb4e4882e8e402b52676cc7
Brett Hawson <brett.hawson@gmail.com>
2021-09-07 17:50:28 +0100

    v1.0.0

---
+++ tap_thinkific/client.py
@@ -0,0 +1,72 @@
+"""REST client handling, including ThinkificStream base class."""
+
+import requests
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from singer_sdk.helpers.jsonpath import extract_jsonpath
+from singer_sdk.streams import RESTStream
+from .auth import ThinkificAuthenticator
+
+
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+
+
+class ThinkificStream(RESTStream):
+    """Thinkific stream class."""
+
+    @property
+    def url_base(self) -> str:
+        """Return the API URL root, configurable via tap settings."""
+        return self.config["api_url"]
+
+    records_jsonpath = "$.items[*]"  # Or override `parse_response`.
+    next_page_token_jsonpath = "$.meta.pagination.next_page"  # Or override `get_next_page_token`.
+
+    @property
+    def http_headers(self) -> dict:
+        """Return the http headers needed."""
+        headers = {}
+        headers["X-Auth-API-Key"] = self.config.get("auth_token")
+        headers["X-Auth-Subdomain"] = self.config.get("subdomain")
+        headers["Content-Type"] = "application/json"
+        return headers
+
+    def get_next_page_token(
+        self, response: requests.Response, previous_token: Optional[Any]
+    ) -> Optional[Any]:
+        """Return a token for identifying next page or None if no more pages."""
+        # TODO: If pagination is required, return a token which can be used to get the
+        #       next page. If this is the final page, return "None" to end the
+        #       pagination loop.
+        if self.next_page_token_jsonpath:
+            all_matches = extract_jsonpath(
+                self.next_page_token_jsonpath, response.json()
+            )
+            first_match = next(iter(all_matches), None)
+            next_page_token = first_match
+        else:
+            next_page_token = response.headers.get("X-Next-Page", None)
+
+        return next_page_token
+
+    def get_url_params(
+        self, context: Optional[dict], next_page_token: Optional[Any]
+    ) -> Dict[str, Any]:
+        """Return a dictionary of values to be used in URL parameterization."""
+        params: dict = {}
+        if next_page_token:
+            params["page"] = next_page_token
+        if self.replication_key:
+            params["limit"] = 1000
+        return params
+
+    def parse_response(self, response: requests.Response) -> Iterable[dict]:
+        """Parse the response and return an iterator of result rows."""
+        # TODO: Parse response body and return a set of records.
+        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
+
+    def post_process(self, row: dict, context: Optional[dict]) -> dict:
+        """As needed, append or transform raw data to match expected structure."""
+        # TODO: Delete this method if not needed.
+        return row
